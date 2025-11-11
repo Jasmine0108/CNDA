ab018de1a842622d1c94a92a2cde44f4e443d3dd
Jasmine0108 <a0905373664@gmail.com>
2025-11-11 17:27:02 +0800

    fix: change ci_python.ml to no activate version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ class get_pybind_include(object):
 
 ext_modules = [
     Extension(
-        'cnda',                                   # module name(Python import name)
+        'cnda',                                   # module name (Python import name)
         ['python/cnda/binding.cpp'],      
         include_dirs=[
             get_pybind_include(),                 # get include table of contents of pybind11
