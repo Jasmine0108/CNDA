d9f5f5587bd336edc120eea94ec6bd7f50c23686
Jasmine0108 <a0905373664@gmail.com>
2025-11-13 01:03:31 +0800

    fix: add constructor

--- python/cnda/binding.cpp
+++ python/cnda/binding.cpp
@@ -3,6 +3,9 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>      // suport std::vector
 #include <cnda/contiguous_nd.hpp>  // include/cnda/
+import sys
+print(sys.path) 
+print(cnda.__file__) 
 
 //py is the abbrivation of pybind11
 namespace py = pybind11;
@@ -11,7 +14,9 @@ template <typename T>
 // Bind c++ function to python function
 void bind_contiguous_nd(py::module_ &m, const std::string &class_name) {
     //empty binding to ensure compile succeeds and make test fail
-    py::class_<ContiguousND<T>>(m, class_name.c_str());
+    py::class_<ContiguousND<T>>(m, class_name.c_str())
+    //Bind c++ constructor to python __init__
+    .def(py::init<std::vector<std::size_t>>(), py::arg("shape")); // size_t -> python int
 }
 PYBIND11_MODULE(cnda, m) {
     m.doc() = "cnda minimal pybind11 bindings";
