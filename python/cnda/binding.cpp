ea9fdc11cd9bdeacd8e877c379c1bd29e25ff39e
Jasmine0108 <a0905373664@gmail.com>
2025-11-23 02:40:12 +0800

    modify: getitem and setitem function to  allow type int as indice

--- python/cnda/binding.cpp
+++ python/cnda/binding.cpp
@@ -23,17 +23,37 @@ void bind_contiguous_nd(py::module_ &m, const std::string &class_name) {
         .def("data", [](ContiguousND<T> &self) {
             return std::vector<T>(self.data(), self.data() + self.size());
         })
-        .def("__getitem__", [](ContiguousND<T> &self, std::vector<std::size_t> idx) {
-            if (idx.size() == 1) return self(idx[0]);
-            else if (idx.size() == 2) return self(idx[0], idx[1]);
-            else if (idx.size() == 3) return self(idx[0], idx[1], idx[2]);
-            else throw std::runtime_error("Unsupported ndim");
+        // To allow type int, list and tuple as indices
+        .def("__getitem__", [](ContiguousND<T>& self, py::object key) -> T {
+            if (py::isinstance<py::int_>(key)) {
+                std::size_t i = key.cast<std::size_t>();
+                return self(i);
+            }
+            else if (py::isinstance<py::tuple>(key) || py::isinstance<py::list>(key)) {
+                std::vector<std::size_t> idx = key.cast<std::vector<std::size_t>>();
+                if (idx.size() == 1) return self(idx[0]);
+                if (idx.size() == 2) return self(idx[0], idx[1]);
+                if (idx.size() == 3) return self(idx[0], idx[1], idx[2]);
+                throw std::runtime_error("Unsupported ndim");
+            }
+            else {
+                throw std::runtime_error("Unsupported index type");
+            }
         })
-        .def("__setitem__", [](ContiguousND<T> &self, std::vector<std::size_t> idx, T value) {
-            if (idx.size() == 1) self(idx[0]) = value;
-            else if (idx.size() == 2) self(idx[0], idx[1]) = value;
-            else if (idx.size() == 3) self(idx[0], idx[1], idx[2]) = value;
-            else throw std::runtime_error("Unsupported ndim");
+        .def("__setitem__", [](ContiguousND<T>& self, py::object key, T value) {
+            if (py::isinstance<py::int_>(key)) {
+                self(key.cast<std::size_t>()) = value;
+                return;
+            }
+            else if (py::isinstance<py::tuple>(key) || py::isinstance<py::list>(key)) {
+                std::vector<std::size_t> idx = key.cast<std::vector<std::size_t>>();
+                if (idx.size() == 1) self(idx[0]) = value;
+                else if (idx.size() == 2) self(idx[0], idx[1]) = value;
+                else if (idx.size() == 3) self(idx[0], idx[1], idx[2]) = value;
+                else throw std::runtime_error("Unsupported ndim");
+                return;
+            }
+            throw std::runtime_error("Unsupported index type");
         });
 }
 
